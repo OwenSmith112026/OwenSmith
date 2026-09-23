02f9db6c5c67f4af3816d4e9173a56ce79249c5e
OwenSmith112026 <830496@mystma.org>
2026-09-23 13:58:24 -0500

    dataconversion

---
+++ dataconversion.py
@@ -0,0 +1,32 @@
+a = 115 
+a_str = str(a) 
+
+b = 3.14 
+b_str = str(b) 
+
+c = "68" 
+c_int = int(c) 
+
+d = "True" 
+d_bool = d == "True"
+
+e = True 
+e_str = str(e)
+
+f = False 
+f_str = str(f)
+
+g = '10110111' 
+g_int = int(g, 2)
+
+h = "2.54" 
+h_float = float(h)
+
+i = 100 
+i_float = float(i)
+
+j = 10.0 
+j_int = int(j)
+
+k = 254 
+k_bytes = bytes([k])
