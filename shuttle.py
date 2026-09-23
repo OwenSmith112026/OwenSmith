edb9d78f0b978785ef57c165e0018ce1fefc8ca8
OwenSmith112026 <830496@mystma.org>
2026-09-23 13:34:02 -0500

    animal survey

---
+++ shuttle.py
@@ -0,0 +1,24 @@
+
+countdown = input("Enter how many seconds are left until launch")
+oxygen_tanks = input("How many oxygen tanks do you have for the mission? ")
+food_packs = input("Enter the number of food packs you have for the journey: ")
+water_packs = input("How many water packs are stored on the ship? ")
+
+
+print("\nSupply Check")
+print(f"Countdown: {countdown}")
+print(f"Oxygen Tanks: {oxygen_tanks}")
+print(f"Food Packs: {food_packs}")
+print(f"Water Packs: {water_packs}")
+
+
+print("\n--- Critical Update ---")
+oxygen_tanks = input("re-enter and confirm the oxygen tank supply: ")
+
+
+print("\n--- Final Supply Check ---")
+print(f"Countdown: {countdown}")
+print(f"Oxygen Tanks: {oxygen_tanks}")
+print(f"Food Packs: {food_packs}")
+print(f"Water Packs: {water_packs}")
+
